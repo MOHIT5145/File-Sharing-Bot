695bd38945818856709713450a3c2a3edbadf3b2
MOHIT5145 <uvrohit004@gmail.com>
2025-06-07 20:30:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from logging.handlers import RotatingFileHandler
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "27693415"))
+API_HASH = os.environ.get("API_HASH", "8dc020d35ff99813b494f20955d8c724")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5783103507"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://cinamaj445:UGdvapVczr1kMEkF@cluster0.zik1uzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "cinamaj445")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
@@ -28,8 +28,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[6848088376]
-    for x in (os.environ.get("ADMINS", "6848088376").split()):
+    ADMINS=[5783103507]
+    for x in (os.environ.get("ADMINS", "5783103507").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -67,7 +67,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need 
 
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6848088376)
+ADMINS.append(5783103507)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
